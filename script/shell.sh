36736afb73befb3e4f0da1703f71e9f10c626835
Patel Dhaval <dhaval.257@gmail.com>
2019-04-19 11:52:50 -0700

    enable tests in travis

--- script/shell.sh
+++ script/shell.sh
@@ -9,5 +9,7 @@ if [ -d "/dev/dri" ]; then
     DEVICE_DIR=--device=/dev/dri:/dev/dri
 fi
 
-sudo docker run $DEVICE_DIR --network=host -v "$DIR/../../../test:/mnt:ro" $(env | grep -E '_(proxy|REPO|VER)=' | sed 's/^/-e /') $(grep '^ARG .*=' "${DIR}/Dockerfile" | sed 's/^ARG /-e /') -it "${IMAGE}" ${*-/bin/bash}
+if [ -z "$TRAVIS" ]; then DOCKER_IT="-it"; else DOCKER_IT=""; fi
+
+sudo docker run $DEVICE_DIR --network=host -v "$DIR/../../../test:/mnt:ro" $(env | grep -E '_(proxy|REPO|VER)=' | sed 's/^/-e /') $(grep '^ARG .*=' "${DIR}/Dockerfile" | sed 's/^ARG /-e /') $DOCKER_IT "${IMAGE}" ${*-/bin/bash}
 
