1ee66d195f895ed961c6584539d6acf897fb4ef0
dpatel257 <dhaval.r.patel@intel.com>
2019-04-18 17:45:25 -0700

    change vieoformat for vaapih264enc test (#76)

--- test/gst_vaapi_h264enc.sh
+++ test/gst_vaapi_h264enc.sh
@@ -6,6 +6,6 @@ case "$1" in
         ;;
     *)
         dd if=/dev/urandom bs=115200 count=300 of=test.yuv # 10 seconds video
-        gst-launch-1.0 -v filesrc location=test.yuv ! videoparse format=i420 width=320 height=240 framerate=30 ! vaapih264enc ! mpegtsmux ! filesink location=test.ts
+        gst-launch-1.0 -v filesrc location=test.yuv ! videoparse format=nv12 width=320 height=240 framerate=30 ! vaapih264enc ! mpegtsmux ! filesink location=test.ts
         ;;
 esac
