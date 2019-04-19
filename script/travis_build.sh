3ec6feee8b819857af75fbba2561e473822df66b
dpatel257 <dhaval.r.patel@intel.com>
2019-04-19 11:57:20 -0700

    Long bug fix in CI (#75)
    * long bug fix and included tests for CI
    
    * fix travis yaml formatting
    
    * fix formatting
    
    * add travis_wait
    
    * Removed tests

---
+++ script/travis_build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+cd $1
+BUILD_NAME=$1
+
+echo Building $BUILD_NAME
+
+BUILD_NAME=$(sed 's/[\/+]/_/g' <<< $BUILD_NAME)
+BUILD_NAME=$(sed 's/[\.\-]//g' <<< $BUILD_NAME)
+
+make >> log_$BUILD_NAME.log
