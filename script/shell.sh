c1ba150fed819567070e4a3877dcf45df963507e
jhou5 <jingdu.hou@intel.com>
2019-05-13 05:05:19 -0700

    Adding condition to check for JENKINS build

--- script/shell.sh
+++ script/shell.sh
@@ -9,7 +9,7 @@ if [ -d "/dev/dri" ]; then
     DEVICE_DIR=--device=/dev/dri:/dev/dri
 fi
 
-if [ -z "$TRAVIS" ]; then DOCKER_IT="-it"; else DOCKER_IT=""; fi
+if [ -z "$TRAVIS" ] || [ -z "$JENKINS_URL" ]; then DOCKER_IT="-it"; else DOCKER_IT=""; fi
 
 sudo docker run $DEVICE_DIR --network=host -v "$DIR/../../../test:/mnt:ro" $(env | grep -E '_(proxy|REPO|VER)=' | sed 's/^/-e /') $(grep '^ARG .*=' "${DIR}/Dockerfile" | sed 's/^ARG /-e /') $DOCKER_IT "${IMAGE}" ${*-/bin/bash}
 
