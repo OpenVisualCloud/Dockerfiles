8267ff0c8f59a4c3ff4931cdc8fa40878771f787
jhou5 <33681822+jhou5@users.noreply.github.com>
2019-06-05 15:59:12 -0700

    fix for ctest doesn't really run issue (#134)

--- script/shell.sh
+++ script/shell.sh
@@ -13,5 +13,12 @@ fi
 
 if [ -z "$TRAVIS" ] && [ -z "$JENKINS_URL" ]; then DOCKER_IT="-it"; else DOCKER_IT=""; fi
 
-sudo docker run $DEVICE_DIR --network=host -v "$DIR/../../../test:/mnt:ro" $(env | grep -E '_(proxy|REPO|VER)=' | sed 's/^/-e /') $(grep '^ARG .*=' "${DIR}/Dockerfile" | sed 's/^ARG /-e /') $DOCKER_IT "${PREFIX}/${IMAGE}" ${*-/bin/bash}
+if echo ${IMAGE} | grep -q "dev"; then
+    TEST="${DIR}/../../../test/"
+else
+    TEST="${DIR}/../../../../test/"
+fi
+
+sudo docker run $DEVICE_DIR --network=host -v "${TEST}:/mnt:ro" $(env | grep -E '_(proxy|REPO|VER)=' | sed 's/^/-e /') $(grep '^ARG .*=' "${DIR}/Dockerfile" | sed 's/^ARG /-e /') $DOCKER_IT "${PREFIX}/${IMAGE}" ${*-/bin/bash}
+
 
