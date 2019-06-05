bdbe21a55643bbfc745f67887ceb6c704703acbc
jhou5 <jingdu.hou@intel.com>
2019-06-04 19:12:36 -0700

    fix for ctest doesn't really run issue

--- script/nginx-tests.cmake
+++ script/nginx-tests.cmake
@@ -1,4 +1,4 @@
-file(GLOB tests "${CMAKE_CURRENT_SOURCE_DIR}/../../../test/nginx_*.sh")
+file(GLOB tests "${DockerFiles_SOURCE_DIR}/test/nginx_*.sh")
 foreach(test ${tests})
     get_filename_component(name ${test} NAME_WE)
     add_test(test_${image}_${name} "${CMAKE_CURRENT_SOURCE_DIR}/shell.sh" "/mnt/${name}.sh" "${image}")
