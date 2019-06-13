820cd10eea3e3dadaa61b7ae7bdfe184ee079806
jhou5 <jingdu.hou@intel.com>
2019-06-12 17:21:17 -0700

    add ffprobe test

--- test/ffmpeg_qsv.sh
+++ test/ffmpeg_qsv.sh
@@ -7,6 +7,7 @@ case "$1" in
     *)
         dd if=/dev/urandom bs=115200 count=300 of=test.yuv # 10 seconds video
         ffmpeg -y -init_hw_device qsv=hw -filter_hw_device hw -f rawvideo -pix_fmt yuv420p -s:v 320x240 -i test.yuv -vf hwupload=extra_hw_frames=64,format=qsv -c:v h264_qsv -b:v 5M test.mp4
+        ffprobe -v error -show_streams test.mp4
         ffmpeg -hwaccel qsv -c:v h264_qsv -i test.mp4 -f null /dev/null
         ;;
 esac
--- test/ffmpeg_x264.sh
+++ test/ffmpeg_x264.sh
@@ -2,4 +2,5 @@
 
 dd if=/dev/urandom bs=115200 count=300 of=test.yuv # 10 seconds video
 ffmpeg -f rawvideo -vcodec rawvideo -s 320x240 -r 30 -pix_fmt yuv420p -i test.yuv -c:v libx264 -y test.mp4
+ffprobe -v error -show_streams test.mp4
 ffmpeg -i test.mp4 -vcodec libx264 -f null /dev/null
