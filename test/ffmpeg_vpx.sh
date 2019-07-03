ac7d13d50acd48f032d8da9b9e7b1ad02a6293a8
jhou5 <33681822+jhou5@users.noreply.github.com>
2019-07-02 17:28:38 -0700

    add ffprobe into the tests (#147)
    * add ffprobe test

--- test/ffmpeg_vpx.sh
+++ test/ffmpeg_vpx.sh
@@ -2,4 +2,5 @@
 
 dd if=/dev/urandom bs=115200 count=30 of=test.yuv # 1 second video
 ffmpeg -f rawvideo -vcodec rawvideo -s 320x240 -r 30 -pix_fmt yuv420p -i test.yuv -c:v libvpx-vp9 -y test.webm
+ffprobe -v error -show_streams test.webm
 ffmpeg -i test.webm -vcodec libvpx-vp9 -f null /dev/null
