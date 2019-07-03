29ffa7df854d513b28fd6f0dc7f17c00a54b6369
Qiujiao <qiujiao.wu@intel.com>
2019-07-03 22:38:53 +0800

    Optimize mongod service status and test result check (#167)
    * Optimize mongod service status and test result check
    
    * Modify mongo status check for both Ubuntu and CentOS

--- test/owt_getstream.sh
+++ test/owt_getstream.sh
@@ -9,7 +9,17 @@ esac
 /home/launch.sh
 
 response=$(curl -sb -H "Accept: application/json" http://localhost:3001/rooms | awk '{split($0,a,","); print a[1]}' | awk '{split($0,a,":"); print a[2]}' | tr -d \")
-
-curl -H "Content-Type: application/json" -X POST -d '{"media":{"audio":{"from":"${response}-common"},"video":{"from":"${response}-common"}}}' http://localhost:3001/rooms/${response}/recordings
-
-curl -sb -H http://localhost:3001/rooms/${response}/streams/${response}-common
+echo $response
+recording=$(curl -H "Content-Type: application/json" -X POST -d '{"media":{"audio":{"from":"${response}-common"},"video":{"from":"${response}-common"}}}' http://localhost:3001/rooms/${response}/recordings)
+echo $recording
+result=$(curl -sb -H http://localhost:3001/rooms/${response}/streams/${response}-common)
+echo $result
+if [[ $result == *"video"* ]]; then
+  echo "Get mix stream succeeded!"
+else
+  cd /home/owt/logs
+  grep error . -ir
+  grep warn . -ir
+  echo "Get stream failed!"
+  exit 1
+fi
