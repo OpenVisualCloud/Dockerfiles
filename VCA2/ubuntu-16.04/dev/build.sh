214e038406cbace7532c7261f5d7a1eb7eda1b4b
liz-lawrens <50250393+liz-lawrens@users.noreply.github.com>
2019-07-08 11:40:39 -0700

    Change release version from 1.1 to 1.2 (#169)
    * Change release version from 1.1 to 1.2
    
    * Update README to v1.2

--- VCA2/ubuntu-16.04/dev/build.sh
+++ VCA2/ubuntu-16.04/dev/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 IMAGE="vca2-ubuntu1604-dev"
-VERSION="1.1"
+VERSION="1.2"
 DIR=$(dirname $(readlink -f "$0"))
 
 . "${DIR}/../../../script/build.sh"
--- Xeon/ubuntu-18.04/media/nginx/build.sh
+++ Xeon/ubuntu-18.04/media/nginx/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 IMAGE="xeon-ubuntu1804-media-nginx"
-VERSION="1.1"
+VERSION="1.2"
 DIR=$(dirname $(readlink -f "$0"))
 
 . "${DIR}/../../../../script/build.sh"
