1ddc95107d849d754c5420e8fabe1b557728e202
jhou5 <jingdu.hou@intel.com>
2019-10-04 11:35:21 -0700

    upgrade version to v19.10

--- VCA2/ubuntu-16.04/dev/build.sh
+++ VCA2/ubuntu-16.04/dev/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 IMAGE="vca2-ubuntu1604-dev"
-VERSION="1.2"
+VERSION="19.10"
 DIR=$(dirname $(readlink -f "$0"))
 
 . "${DIR}/../../../script/build.sh"
--- Xeon/ubuntu-18.04/graphics/ospray-mpi/build.sh
+++ Xeon/ubuntu-18.04/graphics/ospray-mpi/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 IMAGE="xeon-ubuntu1804-graphics-ospary-mpi"
-VERSION="1.2"
+VERSION="19.10"
 DIR=$(dirname $(readlink -f "$0"))
 
 . "${DIR}/../../../../script/build.sh"
