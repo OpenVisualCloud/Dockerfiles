bcb5240b09644a7b4732624baae4909ada89ed9e
jhou5 <jingdu.hou@intel.com>
2019-10-04 10:44:08 -0700

    fix cmake bug found by travisCI

--- script/platform.cmake
+++ script/platform.cmake
@@ -1,5 +1,8 @@
-file(GLOB dirs "*")
-foreach(dir ${dirs})
+if(NOT DEFINED os_dirs)
+    file(GLOB os_dirs "*")
+endif()
+
+foreach(dir ${os_dirs})
     if(EXISTS ${dir}/CMakeLists.txt)
         add_subdirectory(${dir})
     endif()
