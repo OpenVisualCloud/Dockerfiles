c6bbe0df186063f57c34523cdf70203eccc4bde5
Liz <50250393+liz-lawrens@users.noreply.github.com>
2019-11-13 13:41:18 -0800

    Fix for issue 211 - test cases related to build options(i.e.-DBUILD_MP3LAME=OFF, -DBUILD_FDKAAC=OFF) need to be auto excluded from the ctest (#241)

--- script/gst-tests.cmake
+++ script/gst-tests.cmake
@@ -1,5 +1,11 @@
 file(GLOB tests "${DockerFiles_SOURCE_DIR}/test/gst_*.sh")
 foreach(test ${tests})
     get_filename_component(name ${test} NAME_WE)
+    if(NOT ${BUILD_MP3LAME} AND ${name} STREQUAL "gst_lamemp3")
+    #Do not add the gst_lamemp3 test if the flag BUILD_MP3LAME is OFF
+    elseif(NOT ${BUILD_FDKAAC} AND ${name} STREQUAL "gst_fdkaac")
+    #Do not add the gst_fdkaac test if the flag BUILD_FDKAAC is OFF
+    else()
     add_test(test_${image}_${name} "${CMAKE_CURRENT_SOURCE_DIR}/shell.sh" "/mnt/${name}.sh" "${image}")
+    endif()
 endforeach()
