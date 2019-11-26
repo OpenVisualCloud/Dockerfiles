d901cfdb9b9e7b78a2ea55d8dad0315169d7e41e
jhou5 <jingdu.hou@intel.com>
2019-11-26 00:30:11 -0800

    remove svt test from vcac-a images

--- script/ffmpeg-tests.cmake
+++ script/ffmpeg-tests.cmake
@@ -1,7 +1,7 @@
 file(GLOB tests "${DockerFiles_SOURCE_DIR}/test/ffmpeg_*.sh")
 foreach(test ${tests})
     get_filename_component(name ${test} NAME_WE)
-    if("${image}" MATCHES "vcaca_ffmpeg_gst_devel" AND ${name} MATCHES "ffmpeg_svt")
+    if("${image}" MATCHES "vcaca_ffmpeg_gst_devel" AND ${name} MATCHES "(svt|hevc|av1?)")
     #Do not add svt test for VCACA DEV image
     else()
     add_test(test_${image}_${name} "${CMAKE_CURRENT_SOURCE_DIR}/shell.sh" "/mnt/${name}.sh" "${image}")
