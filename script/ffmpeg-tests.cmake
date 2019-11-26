004cc4ef9c0b72d4e74dcc976867c6181e444dc6
jhou5 <jingdu.hou@intel.com>
2019-11-26 00:17:09 -0800

    comment out svt test from VCAC-A images

--- script/ffmpeg-tests.cmake
+++ script/ffmpeg-tests.cmake
@@ -1,5 +1,9 @@
 file(GLOB tests "${DockerFiles_SOURCE_DIR}/test/ffmpeg_*.sh")
 foreach(test ${tests})
     get_filename_component(name ${test} NAME_WE)
+    if("${image}" MATCHES "vcaca_ffmpeg_gst_devel" AND ${name} MATCHES "ffmpeg_svt")
+    #Do not add svt test for VCACA DEV image
+    else()
     add_test(test_${image}_${name} "${CMAKE_CURRENT_SOURCE_DIR}/shell.sh" "/mnt/${name}.sh" "${image}")
+    endif()
 endforeach()
--- script/gst-tests.cmake
+++ script/gst-tests.cmake
@@ -5,6 +5,8 @@ foreach(test ${tests})
     #Do not add the gst_lamemp3 test if the flag BUILD_MP3LAME is OFF
     elseif(NOT ${BUILD_FDKAAC} AND ${name} STREQUAL "gst_fdkaac")
     #Do not add the gst_fdkaac test if the flag BUILD_FDKAAC is OFF
+    elseif(${image} MATCHES "vcaca_ffmpeg_gst_devel" AND ${name} MATCHES "gst_svt")
+    #Do not add svt test for VCACA DEV image
     else()
     add_test(test_${image}_${name} "${CMAKE_CURRENT_SOURCE_DIR}/shell.sh" "/mnt/${name}.sh" "${image}")
     endif()
