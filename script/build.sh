451eb40ddf2a802d8547005990f7dd6c1051d96b
jhou5 <33681822+jhou5@users.noreply.github.com>
2020-01-16 14:45:27 -0800

    faster travis CI through docker hub cache (#346)
     faster travis CI through docker hub cache

--- script/build.sh
+++ script/build.sh
@@ -7,14 +7,19 @@ fi
 BUILD_MP3LAME="${1:-ON}"
 BUILD_FDKAAC="${2:-ON}"
 ONLY_DOCKERFILES="${3:-OFF}"
-
+DOCKER_PREFIX="${4:-openvisualcloud}"
 TEMPLATE="${DIR}/../../../../template/"
+BUILD_CACHE=""
+FULL_CACHE=""
 
-PREFIX="${PREFIX:-openvisualcloud}"
+if test "${DOCKER_PREFIX}" = 'ovccache'; then
+    BUILD_CACHE="--cache-from "${DOCKER_PREFIX}/${IMAGE}:build""
+    FULL_CACHE="--cache-from "${DOCKER_PREFIX}/${IMAGE}:build" --cache-from "${DOCKER_PREFIX}/${IMAGE}:latest""
+fi
 
 for m4file in "${DIR}"/*.m4; do
     if test -f "$m4file"; then
-       m4 "-I${TEMPLATE}" -DDOCKER_IMAGE=${IMAGE} -DBUILD_MP3LAME=${BUILD_MP3LAME} -DBUILD_FDKAAC=${BUILD_FDKAAC} "${m4file}" > "${m4file%\.m4}"
+        m4 "-I${TEMPLATE}" -DDOCKER_IMAGE=${IMAGE} -DBUILD_MP3LAME=${BUILD_MP3LAME} -DBUILD_FDKAAC=${BUILD_FDKAAC} "${m4file}" > "${m4file%\.m4}"
     fi
 done || echo
 
@@ -23,9 +28,9 @@ if test "$1" = '-n'; then
 fi
 
 if test "${ONLY_DOCKERFILES}" = 'OFF'; then
-  if grep -q 'AS build' "${DIR}/Dockerfile"; then
-      sudo -E docker build --network=host --target build -t "${PREFIX}/${IMAGE}:build" "$DIR" $(env | cut -f1 -d= | grep -E '_(proxy|REPO|VER)$' | sed 's/^/--build-arg /')
-  fi
+    if grep -q 'AS build' "${DIR}/Dockerfile"; then
+       sudo -E docker build --network=host ${BUILD_CACHE} --target build -t "${DOCKER_PREFIX}/${IMAGE}:build" "$DIR" $(env | cut -f1 -d= | grep -E '_(proxy|REPO|VER)$' | sed 's/^/--build-arg /')
+    fi
 
-  sudo -E docker build --network=host -t "${PREFIX}/${IMAGE}:${VERSION}" -t "${PREFIX}/${IMAGE}:latest" "$DIR" $(env | cut -f1 -d= | grep -E '_(proxy|REPO|VER)$' | sed 's/^/--build-arg /')
+    sudo -E docker build --network=host ${FULL_CACHE} -t "${DOCKER_PREFIX}/${IMAGE}:${VERSION}" -t "${DOCKER_PREFIX}/${IMAGE}:latest" "$DIR" $(env | cut -f1 -d= | grep -E '_(proxy|REPO|VER)$' | sed 's/^/--build-arg /')
 fi
