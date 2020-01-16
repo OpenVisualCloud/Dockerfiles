5d06c1b9c2ef597675a763f6d0f3a2d567ddbcd5
jhou5 <jingdu.hou@intel.com>
2020-01-16 09:42:34 -0800

    Fix CI test wrong prefix issue

--- script/image.cmake
+++ script/image.cmake
@@ -2,5 +2,5 @@ add_custom_target(build_${image} ALL "${CMAKE_CURRENT_SOURCE_DIR}/build.sh" ${BU
 if(dep_image)
     add_dependencies(build_${image} build_${dep_image})
 endif()
-add_custom_target(shell_${image} "${CMAKE_CURRENT_SOURCE_DIR}/shell.sh")
+add_custom_target(shell_${image} "${CMAKE_CURRENT_SOURCE_DIR}/shell.sh" ${DOCKER_PREFIX})
 
