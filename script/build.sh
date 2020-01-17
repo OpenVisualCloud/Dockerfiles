4661b887120fe14b854e242a982ddc59c0a8ccd2
Christopher Degawa <ccom@randomderp.com>
2020-01-17 18:03:34 +0000

    script: Use bash double bracket

--- script/build.sh
+++ script/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-if test -z "${DIR}"; then 
+if [[ -z $DIR ]]; then
     echo "This script should not be called directly."
     exit 1
 fi
@@ -12,22 +12,22 @@ TEMPLATE="${DIR}/../../../../template/"
 BUILD_CACHE=""
 FULL_CACHE=""
 
-if test "${DOCKER_PREFIX}" = 'ovccache'; then
-    BUILD_CACHE="--cache-from "${DOCKER_PREFIX}/${IMAGE}:build""
-    FULL_CACHE="--cache-from "${DOCKER_PREFIX}/${IMAGE}:build" --cache-from "${DOCKER_PREFIX}/${IMAGE}:latest""
+if [[ ${DOCKER_PREFIX} == ovccache ]]; then
+    BUILD_CACHE="--cache-from ${DOCKER_PREFIX}/${IMAGE}:build"
+    FULL_CACHE="--cache-from ${DOCKER_PREFIX}/${IMAGE}:build --cache-from ${DOCKER_PREFIX}/${IMAGE}:latest"
 fi
 
 for m4file in "${DIR}"/*.m4; do
-    if test -f "$m4file"; then
+    if [[ -f $m4file ]]; then
         m4 "-I${TEMPLATE}" -DDOCKER_IMAGE=${IMAGE} -DBUILD_MP3LAME=${BUILD_MP3LAME} -DBUILD_FDKAAC=${BUILD_FDKAAC} "${m4file}" > "${m4file%\.m4}"
     fi
 done || echo
 
-if test "$1" = '-n'; then 
-    exit 0; 
+if [[ $1 == -n ]]; then
+    exit 0
 fi
 
-if test "${ONLY_DOCKERFILES}" = 'OFF'; then
+if [[ ${ONLY_DOCKERFILES} == OFF ]]; then
     if grep -q 'AS build' "${DIR}/Dockerfile"; then
        sudo -E docker build --network=host ${BUILD_CACHE} --target build -t "${DOCKER_PREFIX}/${IMAGE}:build" "$DIR" $(env | cut -f1 -d= | grep -E '_(proxy|REPO|VER)$' | sed 's/^/--build-arg /')
     fi
--- script/shell.sh
+++ script/shell.sh
@@ -1,21 +1,21 @@
 #!/bin/bash -e
 
-if test -z "${DIR}"; then
+if [[ -z $DIR ]]; then
     echo "This script should not be called directly."
     exit 1
 fi
 
 DOCKER_PREFIX="${DOCKER_PREFIX:-openvisualcloud}"
 
-if [ -d "/dev/dri" ]; then
+if [[ -d /dev/dri ]]; then
     DEVICE_DIR=--device=/dev/dri:/dev/dri
 fi
 
-if [ -z "$TRAVIS" ] && [ -z "$JENKINS_URL" ]; then DOCKER_IT="-it"; else DOCKER_IT=""; fi
+if [[ -z $TRAVIS && -z $JENKINS_URL ]]; then DOCKER_IT="-it"; else DOCKER_IT=""; fi
 
 TEST="${DIR}/../../../../test/"
 
-if echo ${IMAGE} | grep -q "vcaca"; then
+if [[ $IMAGE == *vcaca* ]]; then
     sudo -E docker run $DEVICE_DIR --rm --user root --privileged -v /var/tmp:/var/tmp -v "${TEST}:/mnt:ro" $(env | cut -f1 -d= | grep -E '_(proxy|REPO|VER)$' | sed 's/^/-e /') $(grep '^ARG .*=' "${DIR}/Dockerfile" | sed 's/^ARG \([^=]*\)=.*/-e \1/') $DOCKER_IT "${DOCKER_PREFIX}/${IMAGE}" ${*-/bin/bash}
 else
     sudo -E docker run $DEVICE_DIR --rm -v "${TEST}:/mnt:ro" $(env | cut -f1 -d= | grep -E '_(proxy|REPO|VER)$' | sed 's/^/-e /') $(grep '^ARG .*=' "${DIR}/Dockerfile" | sed 's/^ARG \([^=]*\)=.*/-e \1/') $DOCKER_IT "${DOCKER_PREFIX}/${IMAGE}" ${*-/bin/bash}
