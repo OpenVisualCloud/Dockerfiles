13569fb5599fd77291046a8484c3a07e9dd34751
Christopher Degawa <ccom@randomderp.com>
2020-01-22 15:49:01 -0500

    Scripts: Use some bashisms and quote some variables to prevent globbing (#149)
    * scripts: Don't exit with negative value
    
    * script: Use bash double bracket
    
    * script: Set template and test dirs relative to script
    
    * build.sh: Set build-arg s once
    
    * build.sh: Use @ variable to prevent shell splitting
    
    * travis_build: Quote variables to prevent shell splitting and globbing
    
    * shell: Use true command to shortten DOCKER_PREFIX setting

--- script/build.sh
+++ script/build.sh
@@ -1,36 +1,37 @@
 #!/bin/bash -e
-if test -z "${DIR}"; then 
+if [[ -z $DIR ]]; then
     echo "This script should not be called directly."
-    exit -1
-fi 
+    exit 1
+fi
 
 BUILD_MP3LAME="${1:-ON}"
 BUILD_FDKAAC="${2:-ON}"
 ONLY_DOCKERFILES="${3:-OFF}"
 DOCKER_PREFIX="${4:-openvisualcloud}"
-TEMPLATE="${DIR}/../../../../template/"
+TEMPLATE="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)/../template/"
 BUILD_CACHE=""
 FULL_CACHE=""
 
-if test "${DOCKER_PREFIX}" = 'ovccache'; then
-    BUILD_CACHE="--cache-from "${DOCKER_PREFIX}/${IMAGE}:build""
-    FULL_CACHE="--cache-from "${DOCKER_PREFIX}/${IMAGE}:build" --cache-from "${DOCKER_PREFIX}/${IMAGE}:latest""
+if [[ ${DOCKER_PREFIX} == ovccache ]]; then
+    BUILD_CACHE="--cache-from ${DOCKER_PREFIX}/${IMAGE}:build"
+    FULL_CACHE="--cache-from ${DOCKER_PREFIX}/${IMAGE}:build --cache-from ${DOCKER_PREFIX}/${IMAGE}:latest"
 fi
 
 for m4file in "${DIR}"/*.m4; do
-    if test -f "$m4file"; then
+    if [[ -f $m4file ]]; then
         m4 "-I${TEMPLATE}" -DDOCKER_IMAGE=${IMAGE} -DBUILD_MP3LAME=${BUILD_MP3LAME} -DBUILD_FDKAAC=${BUILD_FDKAAC} "${m4file}" > "${m4file%\.m4}"
     fi
-done || echo
+done || true
 
-if test "$1" = '-n'; then 
-    exit 0; 
+if [[ $1 == -n ]]; then
+    exit 0
 fi
 
-if test "${ONLY_DOCKERFILES}" = 'OFF'; then
+if [[ ${ONLY_DOCKERFILES} == OFF ]]; then
+    build_args=$(env | grep -E '_(proxy|REPO|VER)$' | sed 's/^/--build-arg /')
     if grep -q 'AS build' "${DIR}/Dockerfile"; then
-       sudo -E docker build --network=host ${BUILD_CACHE} --target build -t "${DOCKER_PREFIX}/${IMAGE}:build" "$DIR" $(env | cut -f1 -d= | grep -E '_(proxy|REPO|VER)$' | sed 's/^/--build-arg /')
+        sudo -E docker build --network=host ${BUILD_CACHE} --target build -t "${DOCKER_PREFIX}/${IMAGE}:build" "$DIR" $build_args
     fi
 
-    sudo -E docker build --network=host ${FULL_CACHE} -t "${DOCKER_PREFIX}/${IMAGE}:${VERSION}" -t "${DOCKER_PREFIX}/${IMAGE}:latest" "$DIR" $(env | cut -f1 -d= | grep -E '_(proxy|REPO|VER)$' | sed 's/^/--build-arg /')
+    sudo -E docker build --network=host ${FULL_CACHE} -t "${DOCKER_PREFIX}/${IMAGE}:${VERSION}" -t "${DOCKER_PREFIX}/${IMAGE}:latest" "$DIR" $build_args
 fi
