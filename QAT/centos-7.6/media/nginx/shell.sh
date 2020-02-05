5be46163959312c06b9058aa55653d67ff53e47c
xx yy <xx@yy.com>
2020-02-05 13:07:54 -0800

    relax a bit and mount the devices

--- QAT/centos-7.6/media/nginx/shell.sh
+++ QAT/centos-7.6/media/nginx/shell.sh
@@ -2,5 +2,14 @@
 
 IMAGE="qat-centos76-media-nginx"
 DIR=$(dirname $(readlink -f "$0"))
+for device in /dev/qat* /dev/usdm_drv /dev/uio*; do
+    DEVICE_DIR="--device=$device:$device $DEVICE_DIR"
+done
+
+for tmp in /var/tmp/shm_key_uio_ctl_lock; do
+    DEVICE_DIR="-v $tmp:$tmp $DEVICE_DIR"
+done
+
+DEVICE_DIR="--privileged"
 
 . "${DIR}/../../../../script/shell.sh"
--- template/qat-driver.m4
+++ template/qat-driver.m4
@@ -1,3 +1,3 @@
 # load qat driver
 ADD qat-driver.tar.gz /
-RUN tar cf - /opt/intel/QAT/build | (mkdir -p /home/build && cd /home/build && tar xf -)
+RUN tar cf - /opt/intel/QAT | (mkdir -p /home/build && cd /home/build && tar xf -)
