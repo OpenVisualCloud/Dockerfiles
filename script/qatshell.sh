75dd5b496a8b0ef132b4db57c6d9f69a1a38235e
xx yy <xx@yy.com>
2020-02-07 15:22:44 -0800

    simplify device mount

--- script/qatshell.sh
+++ script/qatshell.sh
@@ -5,5 +5,5 @@ if [[ -z $DIR ]]; then
     exit 1
 fi
 
-DEVICE_DIR="-v /dev/hugepages:/dev/hugepages -v /var/tmp:/var/tmp $(ls -1 /etc/*_dev?.conf | sed 's/\(.*\)/-v \1:\1/') $(ls -1 /dev/uio* /dev/qat_* /dev/usdm_drv | sed 's/\(.*\)/--device=\1:\1/')"
+DEVICE_DIR="$(ls -1 -d /etc/*_dev?.conf /dev/hugepages | sed 's/\(.*\)/-v \1:\1/') $(ls -1 /dev/uio* /dev/qat_* /dev/usdm_drv | sed 's/\(.*\)/--device=\1:\1/')"
 . "${DIR}/../../../../script/shell.sh"
