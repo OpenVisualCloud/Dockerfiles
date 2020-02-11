3129aefae79c9f65c3845fb1a97c7e947813d8fb
jhou5 <jingdu.hou@intel.com>
2020-02-10 21:31:26 -0800

    recover nginx hevc test cases

--- test/nginx_hls_hevc.sh
+++ test/nginx_hls_hevc.sh
@@ -10,5 +10,5 @@ case "$1" in
 esac 
 
 dd if=/dev/urandom bs=115200 count=300 of=test.yuv # 10 seconds video
-#ffmpeg -re -f rawvideo -vcodec rawvideo -s 320x240 -r 30 -pix_fmt yuv420p -i test.yuv -c:v libsvt_hevc -f flv rtmp://localhost/hls/test
-#curl -sSf http://127.0.0.1/hls/test/index.m3u8
+ffmpeg -re -f rawvideo -vcodec rawvideo -s 320x240 -r 30 -pix_fmt yuv420p -i test.yuv -c:v libsvt_hevc -f flv rtmp://localhost/hls/test
+curl -sSf http://localhost/hls/test/index.m3u8
--- test/nginx_rtmp_hevc.sh
+++ test/nginx_rtmp_hevc.sh
@@ -9,9 +9,9 @@ case "$1" in
         yum install -y -q curl;;
 esac
 
-curl -sSf http://127.0.0.1/stat
+curl -sSf http://localhost/stat
 dd if=/dev/urandom bs=115200 count=300 of=test.yuv # 10 seconds video
-#ffmpeg -i rtmp://127.0.0.1/stream/test -vcodec copy -acodec copy -f flv test.flv < /dev/null &
-#ffmpeg -re -f rawvideo -vcodec rawvideo -s 320x240 -r 30 -pix_fmt yuv420p -i test.yuv -c:v libsvt_hevc -f flv rtmp://127.0.0.1/stream/test
-#sleep 1
-#if test -f test.flv; then exit 0; else exit -1; fi
+ffmpeg -i rtmp://localhost/stream/test -vcodec copy -acodec copy -f flv test.flv < /dev/null &
+ffmpeg -re -f rawvideo -vcodec rawvideo -s 320x240 -r 30 -pix_fmt yuv420p -i test.yuv -c:v libsvt_hevc -f flv rtmp://localhost/stream/test
+sleep 1
+if test -f test.flv; then exit 0; else exit -1; fi
