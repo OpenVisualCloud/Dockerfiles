59836de125fce4f427816c1944117df3e73f78a7
Alek Du <alek.du@intel.com>
2020-02-13 14:56:34 +0800

    hddl: remove the ov_hddl_run privileged requirement
    * Pass dynamic device by device-cgroup-rules
    * use docker-compose 2.4 schema for device-cgroup-rules support
    * fix the modprobe missing "-a" issue for multiple modules
    
    Signed-off-by: Alek Du <alek.du@intel.com>

--- VCAC-A/ubuntu-18.04/analytics/hddldaemon/init_hddl.sh
+++ VCAC-A/ubuntu-18.04/analytics/hddldaemon/init_hddl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-modprobe i2c-dev i2c-i801 i2c-hid
-cd /opt/intel/openvino/deployment_tools/inference_engine/external/hddl/drivers
-bash setup.sh install
-
+modprobe -a i2c-dev i2c-i801 i2c-hid myd_ion myd_vsc
+if [ ! -c /dev/ion ]; then
+   cd /opt/intel/openvino/deployment_tools/inference_engine/external/hddl/drivers
+   bash setup.sh install
+fi
