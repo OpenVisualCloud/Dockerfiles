c68ce483680947db4a08e25d210aba18ff877ed0
jhou5 <jingdu.hou@intel.com>
2020-02-26 13:54:08 -0800

    more

--- script/travis_build.sh
+++ script/travis_build.sh
@@ -3,11 +3,11 @@
 echo "where are we" && pwd
 cd "$1" || exit 1
 BUILD_NAME=$1
-
+BIULD_LOG=/home/travis/travis.log
 echo "Building $BUILD_NAME"
 
 BUILD_NAME=$(sed 's/[\/+]/_/g' <<< $BUILD_NAME)
 BUILD_NAME=$(sed 's/[\.\-]//g' <<< $BUILD_NAME)
 
-make >> /home/travis/travis.log
-ctest >> /home/travis//travis.log 
+make >> $BUILD_LOG 2>&1|| exit 1
+ctest >> $BUILD_LOG 2>&1 || ctest -V >> $BUILD_LOG 2>&1
