61cd0055ebcd1f65812997bc4398458675e3c913
jhou5 <jingdu.hou@intel.com>
2020-02-26 15:34:45 -0800

    11

--- script/travis_build.sh
+++ script/travis_build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -e
 
 BUILD_NAME=$1
+BUILD_ROOT=`pwd`
+BUILD_LOG=${BUILD_ROOT}/travis.log
 
-cd "$BUILD_NAME" || exit 1
-make || exit 1
-ctest || ctest -V --rerun-failed
+echo BUILD_ROOT=${BUILD_ROOT}
+echo BUILD_LOG=${BUILD_LOG}
+cd "$BUILD_NAME" > ${BUILD_LOG} || exit 1
+make >> ${BUILD_LOG} || exit 1
+ctest >> ${BUILD_LOG} || ctest -V --rerun-failed >> ${BUILD_LOG}
