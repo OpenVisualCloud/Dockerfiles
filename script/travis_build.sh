5d5db6bf86dabfbec28f4b48399d122eb4f75511
jhou5 <jingdu.hou@intel.com>
2020-02-26 14:35:00 -0800

    new way

--- script/travis_build.sh
+++ script/travis_build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash -e
 
 BUILD_NAME=$1
-BUILD_ROOT=`pwd`
-BUILD_LOG=${BUILD_ROOT}/travis.log
-echo "BUILD_LOG=$BUILD_LOG"
-echo "Building $BUILD_NAME"
-cd "$1" || exit 1
-make >> $BUILD_LOG || exit 1
-ctest >> $BUILD_LOG || ctest -V >> $BUILD_LOG 
+
+cd "$BUILD_NAME" || exit 1
+make || exit 1
+ctest || ctest -V  
