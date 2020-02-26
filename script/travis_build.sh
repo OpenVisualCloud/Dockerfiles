b50f136d92c23de4504c38b5b83dd9ee942c03ee
jhou5 <jingdu.hou@intel.com>
2020-02-26 11:20:44 -0800

    fi

--- script/travis_build.sh
+++ script/travis_build.sh
@@ -8,4 +8,5 @@ echo "Building $BUILD_NAME"
 BUILD_NAME=$(sed 's/[\/+]/_/g' <<< $BUILD_NAME)
 BUILD_NAME=$(sed 's/[\.\-]//g' <<< $BUILD_NAME)
 
-make >> "log_$BUILD_NAME.log" && ctest >> "log_$BUILD_NAME.log"
+make >> "travis.log" || exit 1 
+ctest >> "travis.log" || exit 1
