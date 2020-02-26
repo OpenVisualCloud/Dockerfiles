22e7b76d59366ccf7a05a43e027ddfbad0a7c21d
jhou5 <jingdu.hou@intel.com>
2020-02-26 14:03:38 -0800

    add fix

--- script/travis_build.sh
+++ script/travis_build.sh
@@ -1,13 +1,10 @@
 #!/bin/bash -e
 
-echo "where are we" && pwd
-cd "$1" || exit 1
 BUILD_NAME=$1
-BIULD_LOG=/home/travis/travis.log
+BUILD_ROOT=`pwd`
+BIULD_LOG=${BUILD_ROOT}/travis.log
+
 echo "Building $BUILD_NAME"
-
-BUILD_NAME=$(sed 's/[\/+]/_/g' <<< $BUILD_NAME)
-BUILD_NAME=$(sed 's/[\.\-]//g' <<< $BUILD_NAME)
-
+cd "$1" || exit 1
 make >> $BUILD_LOG 2>&1|| exit 1
 ctest >> $BUILD_LOG 2>&1 || ctest -V >> $BUILD_LOG 2>&1
