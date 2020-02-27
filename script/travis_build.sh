7907190d300e9783868fccb3d92446d51441f346
jhou5 <33681822+jhou5@users.noreply.github.com>
2020-02-26 19:13:52 -0800

    Update script/travis_build.sh
    Good suggestion. Thanks!
    
    Co-Authored-By: Christopher Degawa <ccom@randomderp.com>

--- script/travis_build.sh
+++ script/travis_build.sh
@@ -2,6 +2,8 @@
 
 BUILD_LOG="$(pwd)/travis.log"
 
-cd "$1" > ${BUILD_LOG} || exit 1
-make >> ${BUILD_LOG} || exit 1
-(ctest || ctest -V --rerun-failed) >> ${BUILD_LOG} 
+{
+    cd "$1" || exit 1
+    make || exit 1
+    (ctest || ctest -V --rerun-failed) 
+} > "$BUILD_LOG"
