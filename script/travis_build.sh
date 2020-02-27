de880376a51cfec98b9e5f3d4553e3867fc8b58b
jhou5 <jingdu.hou@intel.com>
2020-02-26 16:10:00 -0800

    2

--- script/travis_build.sh
+++ script/travis_build.sh
@@ -2,6 +2,6 @@
 
 BUILD_LOG="$(pwd)/travis.log"
 
-cd "$1" > ${BUILD_LOG} 2>&1 || exit 1
-make >> ${BUILD_LOG} 2>&1 || exit 1
-(ctest || ctest -V --rerun-failed) >> ${BUILD_LOG} 2>&1
+cd "$1" > ${BUILD_LOG} || exit 1
+make >> ${BUILD_LOG} || exit 1
+(ctest || ctest -V --rerun-failed) >> ${BUILD_LOG} 
