5bcef04d6573de0507f833191f4cd82abe70a24e
jhou5 <33681822+jhou5@users.noreply.github.com>
2020-03-10 17:02:19 -0700

    Downstream master fix for libinference_engine_preproc.so & VCAC-A script update (#426)
    * fix script for multiple VCAC-A nodes
    
    * add -o rule
    
    * Openvino r4 test fix (#424)
    
    * doc update for vaapi instructions
    
    * fix vino test bug with manual copy of ie so
    
    * dev image fix and typos
    
    Co-authored-by: dpatel257 <dhaval.r.patel@intel.com>
    Co-authored-by: xwu2git <3871873+xwu2git@users.noreply.github.com>

--- VCAC-A/script/setup_access.sh
+++ VCAC-A/script/setup_access.sh
@@ -23,18 +23,33 @@ case "$0" in
             cat /dev/zero | ssh-keygen -q -N ""
         fi
 
+        # Setup network
+        sudo sysctl -w net.ipv4.ip_forward=1
+
+        # Setup proxy & time
         http_proxy=${http_proxy:-${HTTP_PROXY}}
         https_proxy=${https_proxy:-${HTTPS_PROXY}}
         no_proxy=${no_proxy:-${NO_PROXY}}
 
         NODEUSER="root"
-        for nodeip in $(sudo vcactl network ip 2> /dev/null | grep -E '^[0-9.]+$'); do
+        for nodeinfo in $(sudo vcactl config-show | awk '/host-ip:/{hi=$2}/host-mask:/{hm=$2;print ip","hi"/"hm}$1=="ip:"{ip=$2}'); do
+
+            # Setup network
+            nodenet=$(echo $nodeinfo | cut -f2 -d,)
+            nodeint=$(sudo ip -4 address show | awk -v net=$nodenet '/^[0-9]+:/{name=$2}/inet /&&$2==net{print name}' | cut -f1 -d:)
+            sudo iptables -t nat -A POSTROUTING -s $nodenet -d 0/0 -j MASQUERADE
+            sudo iptables -I FORWARD -j ACCEPT -i $nodeint
+            sudo iptables -I FORWARD -j ACCEPT -o $nodeint
+
+            # Setup address
+            nodeip=$(echo $nodeinfo | cut -f1 -d,)
             ssh-copy-id $NODEUSER@${nodeip} 2> /dev/null || echo
             scp /etc/resolv.conf $NODEUSER@$nodeip:/etc/resolv.conf
             ssh $NODEUSER@$nodeip "date -s \"$(date)\""
             scp "$0" $NODEUSER@$nodeip:/root/install-access.sh
             ssh $NODEUSER@$nodeip /root/install-access.sh "$http_proxy" "$https_proxy" "$no_proxy"
-            ssh $NODEUSER@$nodeip "apt-get update && apt-get install -y dbus && timedatectl set-timezone \"$(timedatectl | awk '/Time zone:/{print$3}')\""
+            ssh $NODEUSER@$nodeip "apt-get update && apt-get install -y dbus tzdata && timedatectl set-timezone \"$(timedatectl | awk '/Time zone:/{print$3}')\""
+
         done
         ;;
 esac
