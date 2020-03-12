a477468138b3150a6d1742b5e3ab8f4f332ffbe2
xwu2git <3871873+xwu2git@users.noreply.github.com>
2020-03-12 10:51:48 -0700

    fix swarm script for multiple VCAC-As

--- VCAC-A/script/setup_swarm.sh
+++ VCAC-A/script/setup_swarm.sh
@@ -2,7 +2,7 @@
 
 # setup host docker swarm if not already
 docker swarm leave --force 2> /dev/null || true
-docker swarm init --advertise-addr=$(sudo vcactl config-show |awk '/gateway/{print$2;exit}') 2>/dev/null || true
+docker swarm init --advertise-addr=${1:-$(sudo vcactl config-show |awk '/gateway/{print$2;exit}')} 2>/dev/null || true
 JOINCMD=$(docker swarm join-token worker | grep token)
 
 NODEUSER="root"
@@ -15,9 +15,9 @@ done
 
 # setup node labels
 for id in $(docker node ls -q 2>/dev/null); do
-    nodeip="$(docker node inspect -f {{.Status.Addr}} $id)"
-    if test -n "$(echo $NODES | grep --fixed-strings $nodeip)"; then
-        echo "label $id: vcac_zone=yes"
+    nodename="$(docker node inspect -f {{.Description.Hostname}} $id)"
+    if test -n "$(sudo vcactl config-show | awk -v name=$nodename '/node-name:/&&$2==name{print 1}')"; then
+        echo "label $nodename: vcac_zone=yes"
         docker node update --label-add vcac_zone=yes $id
     fi
 done
