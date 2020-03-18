5c424f0d9dfa94229602254720f5ba3dad872e11
drpatel <dhaval.r.patel@intel.com>
2020-03-18 15:12:42 -0700

    documentation and cmake option enable

--- script/platform.cmake
+++ script/platform.cmake
@@ -10,3 +10,10 @@ if(platform)
         add_custom_target(build_${platform} ALL COMMAND ${CMAKE_SOURCE_DIR}/script/update-imagelist.sh ${DOCKER_PREFIX} ${platform} WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
     endif()
 endif()
+
+if(platform)
+    if(UPDATE_IMAGE_README STREQUAL "ON")
+        add_custom_target(build_${platform} ALL COMMAND sudo python3 ${CMAKE_SOURCE_DIR}/script/generate_readme.py )
+    endif()
+endif()
+
