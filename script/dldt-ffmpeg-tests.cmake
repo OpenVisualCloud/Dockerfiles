aec974229ba2d442a127deb2a6489f3d372174f9
jhou5 <jingdu.hou@intel.com>
2020-04-27 11:38:48 -0700

    remove VCAC-A test cases from non-VCAC-A platform

--- script/dldt-ffmpeg-tests.cmake
+++ script/dldt-ffmpeg-tests.cmake
@@ -1,10 +1,10 @@
 file(GLOB tests "${DockerFiles_SOURCE_DIR}/test/dldt_ffmpeg_*.sh")
 foreach(test ${tests})
     get_filename_component(name ${test} NAME_WE)
-    if(NOT "${image}" MATCHES "vcaca" AND ${name} MATCHES "vcaa")
-    #Do not add vcaa test to non-vcaa images
-    elseif(("${image}" MATCHES "vcaca") AND NOT (${name} MATCHES "vcaa"))
-    #Do not add non-vcaa video analytics test to vcaa
+    if(NOT "${image}" MATCHES "vcaca" AND ${name} MATCHES "vcaca")
+    #Do not add vcaca test to non-vcaca images
+    elseif(("${image}" MATCHES "vcaca") AND NOT (${name} MATCHES "vcaca"))
+    #Do not add non-vcaca video analytics test to vcaca
     else()
     add_test(test_${image}_${name} "${CMAKE_CURRENT_SOURCE_DIR}/shell.sh" "/mnt/${name}.sh" "${image}")
     endif()
