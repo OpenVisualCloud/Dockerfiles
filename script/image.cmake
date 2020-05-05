468c15a6feffa31e1e110521bd852ed69c92a0f5
xwu2 <3871873+xwu2git@users.noreply.github.com>
2020-05-05 15:11:02 -0700

    misc cmake/make script improvements (#488)
    * move script actions to make commands
    
    * fix platform script
    
    * fix README
    
    * Update doc/cmake.md
    
    Co-authored-by: Christopher Degawa <ccom@randomderp.com>
    
    * make exit code >0
    
    Co-authored-by: Christopher Degawa <ccom@randomderp.com>

--- script/image.cmake
+++ script/image.cmake
@@ -1,6 +1,14 @@
-add_custom_target(build_${image} ALL "${CMAKE_CURRENT_SOURCE_DIR}/build.sh" ${BUILD_VERSION} ${BUILD_MP3LAME} ${BUILD_FDKAAC} ${UPDATE_DOCKERFILES} ${UPDATE_DOCKERHUB_README} ${DOCKER_PREFIX})
+add_custom_target(build_${image} ALL "${CMAKE_CURRENT_SOURCE_DIR}/build.sh" ${BUILD_VERSION} ${BUILD_MP3LAME} ${BUILD_FDKAAC} ${DOCKER_PREFIX})
 if(dep_image)
     add_dependencies(build_${image} build_${dep_image})
 endif()
 add_custom_target(shell_${image} "${CMAKE_CURRENT_SOURCE_DIR}/shell.sh" ${DOCKER_PREFIX})
 
+add_custom_target(update_${image}_dockerfile COMMAND "${CMAKE_CURRENT_SOURCE_DIR}/build.sh" -n ${BUILD_MP3LAME} ${BUILD_FDKAAC} ${DOCKER_PREFIX})
+add_dependencies(update_dockerfile update_${image}_dockerfile)
+
+add_custom_target(generate_${image}_readme COMMAND "${CMAKE_SOURCE_DIR}/script/generate_readme.py" "${CMAKE_CURRENT_SOURCE_DIR}")
+add_dependencies(generate_readme generate_${image}_readme)
+
+add_custom_target(upload_${image}_readme COMMAND "${CMAKE_SOURCE_DIR}/script/upload-dockerhub-readme.sh" ${DOCKER_PREFIX} ${image} "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
+add_dependencies(upload_readme upload_${image}_readme)
