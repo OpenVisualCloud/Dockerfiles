591e7ee5a7745e2f5b872f4b185e88ff2f0c5514
xwu2git <3871873+xwu2git@users.noreply.github.com>
2020-05-04 19:43:11 -0700

    move script actions to make commands

--- doc/cmake.md
+++ doc/cmake.md
@@ -1,5 +1,5 @@
 
-### Customize Build Process:
+### Customize the Build Process:
 
 You can use the following build options to customize the build process:
 - **BUILD_MP3LAME**: Set to ```ON``` (default) to build the ```libmp3lame``` in the docker images, or ```OFF``` to disable including ```libmp3lame``` in the docker images.
@@ -11,20 +11,30 @@ You can use the following build options to customize the build process:
 
 Build examples:   
 
-```
+```bash
 cd build
 cmake -DBUILD_MP3LAME=ON ..
 ```
 
-```
+```bash
 cd build
 cmake -DBUILD_FDKAAC=ON -DBUILD_MP3LAME=OFF ..
 ```
-#### Generate README files:
-`python3` is required on the host system to use this.
 
-    cd build
-    cmake -DUPDATE_IMAGE_README=ON ..
+### Command Make Commands
+
+- **update_link**: Update the image links in the platform README.md.  
+- **update_dockerfile**: Update the Dockerfiles. Do this after modifying any templates.  
+- **generate_readme**: Generate the image README.md for dockerhub.   
+- **upload_readme**: Upload the image README.md to dockerhub.  
+
+Example: Generate the dockerhub READMEs  
+
+```bash
+cd build
+cmake ..
+make generate_readme
+```
 
 ### Use Alternative Repo:
 
@@ -41,6 +51,4 @@ For a list of all REPOs and their versions, run the following command:
 ```bash
 grep -E '_(REPO|VER)=' template/*.m4
 ```
-~
-
 
--- script/build.sh
+++ script/build.sh
@@ -7,9 +7,7 @@ fi
 BUILD_VERSION="${1:-1.0}"
 BUILD_MP3LAME="${2:-ON}"
 BUILD_FDKAAC="${3:-ON}"
-UPDATE_DOCKERFILES="${4:-OFF}"
-UPDATE_DOCKERHUB_README="${5:-OFF}"
-DOCKER_PREFIX="${6:-openvisualcloud}"
+DOCKER_PREFIX="${4:-openvisualcloud}"
 SCRIPT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)/"
 BUILD_CACHE=""
 FULL_CACHE=""
@@ -29,14 +27,9 @@ if [[ $1 == -n ]]; then
     exit 0
 fi
 
-if [[ ${UPDATE_DOCKERFILES} == OFF ]]; then
-    build_args=$(env | cut -f1 -d= | grep -E '_(proxy|REPO|VER)$' | sed 's/^/--build-arg /')
-    if grep -q 'AS build' "${DIR}/Dockerfile"; then
-        docker build --network=host ${BUILD_CACHE} --target build -t "${DOCKER_PREFIX}/${IMAGE}:build" "$DIR" $build_args
-    fi
-
-    docker build --network=host ${FULL_CACHE} -t "${DOCKER_PREFIX}/${IMAGE}:${BUILD_VERSION}" -t "${DOCKER_PREFIX}/${IMAGE}:latest" "$DIR" $build_args
-elif [[ ${UPDATE_DOCKERHUB_README} == ON ]]; then
-    README_FILEPATH="$(echo "$PWD/README.md" | sed 's/build\///')"
-    ${SCRIPT_ROOT}/update-dockerhub-readme.sh ${DOCKER_PREFIX} ${IMAGE} ${README_FILEPATH}
+build_args=$(env | cut -f1 -d= | grep -E '_(proxy|REPO|VER)$' | sed 's/^/--build-arg /')
+if grep -q 'AS build' "${DIR}/Dockerfile"; then
+    docker build --network=host ${BUILD_CACHE} --target build -t "${DOCKER_PREFIX}/${IMAGE}:build" "$DIR" $build_args
 fi
+
+docker build --network=host ${FULL_CACHE} -t "${DOCKER_PREFIX}/${IMAGE}:${BUILD_VERSION}" -t "${DOCKER_PREFIX}/${IMAGE}:latest" "$DIR" $build_args
