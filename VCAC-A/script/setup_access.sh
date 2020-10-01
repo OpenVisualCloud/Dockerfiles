6cb376a11395ec736d095aaa9c5b2a38a685c54f
xwu2git <3871873+xwu2git@users.noreply.github.com>
2020-10-01 13:36:50 -0700

    skip unavailable node(s)

--- VCAC-A/script/setup_access.sh
+++ VCAC-A/script/setup_access.sh
@@ -37,20 +37,22 @@ case "$0" in
             # Setup network
             nodenet=$(echo $nodeinfo | cut -f2 -d,)
             nodeint=$(sudo ip -4 address show | awk -v net=$nodenet '/^[0-9]+:/{name=$2}/inet /&&$2==net{print name}' | cut -f1 -d:)
-            sudo iptables -t nat -A POSTROUTING -s $nodenet -d 0/0 -j MASQUERADE
-            sudo iptables -I FORWARD -j ACCEPT -i $nodeint
-            sudo iptables -I FORWARD -j ACCEPT -o $nodeint
 
-            # Setup address
-            nodeip=$(echo $nodeinfo | cut -f1 -d,)
-            ssh-copy-id $NODEUSER@${nodeip} 2> /dev/null || echo
-            scp /etc/resolv.conf $NODEUSER@$nodeip:/etc/resolv.conf
-            ssh $NODEUSER@$nodeip "date -s \"$(date)\""
-            scp "$0" $NODEUSER@$nodeip:/root/install-access.sh
-            ssh $NODEUSER@$nodeip /root/install-access.sh "$http_proxy" "$https_proxy" "$no_proxy"
-            ssh $NODEUSER@$nodeip "if [ \$(dpkg-query --show | grep '^\(dbus\|tzdata\)' | wc -l) != '2' ]; then apt-get update && apt-get install -y dbus tzdata; fi"
-            ssh $NODEUSER@$nodeip "timedatectl set-timezone \"$(timedatectl | awk '/Time zone:/{print$3}')\""
+            if [ -n "$nodeint" ]; then
+                sudo iptables -t nat -A POSTROUTING -s $nodenet -d 0/0 -j MASQUERADE
+                sudo iptables -I FORWARD -j ACCEPT -i $nodeint
+                sudo iptables -I FORWARD -j ACCEPT -o $nodeint
 
+                # Setup address
+                nodeip=$(echo $nodeinfo | cut -f1 -d,)
+                ssh-copy-id $NODEUSER@${nodeip} 2> /dev/null || echo
+                scp /etc/resolv.conf $NODEUSER@$nodeip:/etc/resolv.conf
+                ssh $NODEUSER@$nodeip "date -s \"$(date)\""
+                scp "$0" $NODEUSER@$nodeip:/root/install-access.sh
+                ssh $NODEUSER@$nodeip /root/install-access.sh "$http_proxy" "$https_proxy" "$no_proxy"
+                ssh $NODEUSER@$nodeip "if [ \$(dpkg-query --show | grep '^\(dbus\|tzdata\)' | wc -l) != '2' ]; then apt-get update && apt-get install -y dbus tzdata; fi"
+                ssh $NODEUSER@$nodeip "timedatectl set-timezone \"$(timedatectl | awk '/Time zone:/{print$3}')\""
+            fi
         done
         ;;
 esac
