bb2cf6f57b20ce6a0d610b062c60a6fedcc8fa8b
jhou5 <jingdu.hou@intel.com>
2020-10-08 14:33:50 -0700

    add centos7 support

---
+++ Xeon/centos-7/service/owt/shell.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+
+IMAGE="xeon-centos7-service-owt"
+DIR=$(dirname $(readlink -f "$0"))
+
+. "${DIR}/../../../../script/shell.sh"
---
+++ XeonE3/centos-7/analytics/gst/shell.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+
+IMAGE="xeone3-centos7-analytics-gst"
+DIR=$(dirname $(readlink -f "$0"))
+
+. "${DIR}/../../../../script/shell.sh"
