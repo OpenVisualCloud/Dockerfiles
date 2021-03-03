78806a19af548d06a77a22c2b74c6d5295b0f8f3
jhou5 <jingdu.hou@intel.com>
2021-03-03 11:15:13 -0800

    remove redundant comments

--- test/owt_getstream.sh
+++ test/owt_getstream.sh
@@ -8,7 +8,6 @@ esac
 
 /home/launch.sh
 
-#response=$(curl -sb -H "Accept: application/json" http://localhost:3001/rooms | awk '{split($0,a,","); print a[1]}' | awk '{split($0,a,":"); print a[2]}' | tr -d \")
 response=$(curl -sb -H "Accept: application/json" http://localhost:3001/rooms | awk -F "," '{print $NF}' | awk '{split($0,a,"\""); print a[4]}')
 echo $response
 recording=$(curl -H "Content-Type: application/json" -X POST -d '{"media":{"audio":{"from":"${response}-common"},"video":{"from":"${response}-common"}}}' http://localhost:3001/rooms/${response}/recordings)
