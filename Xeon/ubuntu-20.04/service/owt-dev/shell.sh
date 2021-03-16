0659664e46288b7687c0e44d70c0b49720d3fbf6
jhou5 <jingdu.hou@intel.com>
2021-03-15 20:50:39 -0700

    remove ubuntu 20.04 owt image temporarily

--- Xeon/ubuntu-20.04/service/owt-dev/shell.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-IMAGE="xeon-ubuntu2004-service-owt-dev"
-DIR=$(dirname $(readlink -f "$0"))
-
-. "${DIR}/../../../../script/shell.sh"
--- Xeon/ubuntu-20.04/service/owt/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-IMAGE="xeon-ubuntu2004-service-owt"
-DIR=$(dirname $(readlink -f "$0"))
-
-. "${DIR}/../../../../script/build.sh"
