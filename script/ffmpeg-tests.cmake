7b146d4cb9b0239e7f13288b4f0a6a6b5809fafb
Patel, Dhaval R <dhaval.r.patel@intel.com>
2021-05-07 11:39:21 -0700

    do not add vmaf for analytics images

--- script/ffmpeg-tests.cmake
+++ script/ffmpeg-tests.cmake
@@ -3,6 +3,8 @@ foreach(test ${tests})
     get_filename_component(name ${test} NAME_WE)
     if("${image}" MATCHES "vcaca" AND ${name} MATCHES "(svt|hevc|av1?)")
     #Do not add svt test for VCACA DEV image
+    elseif(${image} MATCHES "analytics" AND ${name} STREQUAL "ffmpeg_vmaf")
+    #Do not add vmaf test for analytics image
     else()
     add_test(test_${image}_${name} "${CMAKE_CURRENT_SOURCE_DIR}/shell.sh" "/mnt/${name}.sh" "${image}")
     endif()
