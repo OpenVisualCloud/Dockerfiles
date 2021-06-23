5f892acab7043115cdc710ec34736505913f2b1c
Patel, Dhaval R <dhaval.r.patel@intel.com>
2021-06-22 17:10:29 -0700

    v216 tests update - part 1

--- test/ffmpeg_ivf_av1.sh
+++ test/ffmpeg_ivf_av1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 dd if=/dev/urandom bs=115200 count=30 of=test.yuv # 30 frames
-ffmpeg -f rawvideo -vcodec rawvideo -s 320x240 -r 30 -pix_fmt yuv420p -i test.yuv -c:v libsvt_av1 -f ivf -y test.ivf
+ffmpeg -f rawvideo -vcodec rawvideo -s 320x240 -r 30 -pix_fmt yuv420p -i test.yuv -c:v libsvtav1 -f ivf -y test.ivf
