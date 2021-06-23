1646c57d46e530c38d4e0e497a0f240228c05739
dpatel257 <dhaval.r.patel@intel.com>
2021-06-22 17:26:21 -0700

    [master] Test fixes for Release v21.6 - part 1 (#750)
    * Update Docker image README (#704)
    
    * update README
    
    * Update dockerfiles
    
    * v216 tests update - part 1
    
    Co-authored-by: jhou5 <jingdu.hou@intel.com>

--- test/ffmpeg_svt_av1.sh
+++ test/ffmpeg_svt_av1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 dd if=/dev/urandom bs=115200 count=30 of=test.yuv # 30 frames
-ffmpeg -f rawvideo -vcodec rawvideo -s 320x240 -r 30 -pix_fmt yuv420p -i test.yuv -c:v libsvt_av1 -y test.mp4
+ffmpeg -f rawvideo -vcodec rawvideo -s 320x240 -r 30 -pix_fmt yuv420p -i test.yuv -c:v libsvtav1 -y test.mp4
 ffprobe -v error -show_streams test.mp4
